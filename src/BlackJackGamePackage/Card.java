0ca2ad21e928920e1a12936cb9f13b4f26aa7526
Darji Aymane <aymanedarji7@gmail.com>
2022-10-17 15:08:34 +0100

    Create Getters and Setters for the class Card

--- src/BlackJackGamePackage/Card.java
+++ src/BlackJackGamePackage/Card.java
@@ -1,7 +1,7 @@
 package BlackJackGamePackage;
 
 public class Card {
-    private int counter = 0;
+    public static int counter = 0;
     private int id;
     private int cardNumber;
     private int cardSymbole;
@@ -9,6 +9,26 @@ public class Card {
     public Card(int cardNumber, int cardSymbole) {
         this.id = counter++;
         this.cardNumber = cardNumber;
+        this.cardSymbole = cardSymbole;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getCardSymbole() {
+        return cardSymbole;
+    }
+
+    public void setCardSymbole(int cardSymbole) {
         this.cardSymbole = cardSymbole;
     }
 
