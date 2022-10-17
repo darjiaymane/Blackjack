3917592cae9474ace0b169d4a164ef5d449e8567
Darji Aymane <aymanedarji7@gmail.com>
2022-10-17 15:22:32 +0100

    Add the constructor to the Player Class

--- src/BlackJackGamePackage/Player.java
+++ src/BlackJackGamePackage/Player.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Player extends Person{
     public static int counter = 0;
     private int id_player;
-    private List<Card> dealerCarteList = new ArrayList<Card>();
+    private List<Card> dealerCarteList;
 
+    public Player(String name) {
+        super(name);
+        this.id_player = counter++;
+    }
 }
