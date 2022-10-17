d0396ad52eea3c877e1a0099fef4a99ffd6b1a6e
Darji Aymane <aymanedarji7@gmail.com>
2022-10-17 15:20:04 +0100

    Create the class Player and its attributes

---
+++ src/BlackJackGamePackage/Player.java
@@ -0,0 +1,11 @@
+package BlackJackGamePackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player extends Person{
+    public static int counter = 0;
+    private int id_player;
+    private List<Card> dealerCarteList = new ArrayList<Card>();
+
+}
